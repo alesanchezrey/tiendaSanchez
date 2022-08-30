7dfe236ed7819a8eab8352a3315da3f8a0e683c6
alesanchezrey <alesanchezrey@gmail.com>
2022-08-30 16:31:10 -0500

    Entrega Navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,8 +1,18 @@
-import logo from '../logo.svg'
+import logo from '../funkologo.png'
 const NavBar = () => {
     return (
-        <div>
-            <img src={logo} className="App-logo" alt="logo" />
+        <div className='navBarMenu'>
+            <div><img src={logo} className='logo' alt="logo" /></div>
+            <div className='menuButtons'>
+                <ul>
+                    <li><a href="#">Inicio</a></li>
+                    <li><a href="#">Marvel</a></li>
+                    <li><a href="#">DC</a></li>
+                    <li><a href="#">Otros</a></li>
+                    <li><a href="#">Contacto</a></li>
+                 </ul>
+            </div>
+            <div></div>
         </div>
     )
 }
